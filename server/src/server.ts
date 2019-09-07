6937e6731e56f9a67ccdc05d27eaff73ce8610f2
Pat Myron <PatMyron@users.noreply.github.com>
2019-09-06 21:32:28 -0700

    improving CloudFormation Template detection
    https://github.com/awslabs/aws-cfn-lint-visual-studio-code/pull/59

--- server/src/server.ts
+++ server/src/server.ts
@@ -129,7 +129,7 @@ function isCloudFormation(template: string, filename: string): Boolean {
 			". Found the string AWSTemplateFormatVersion");
 		return true;
 	}
-	if (/\n"?Resources"?\s*:/.exec(template)) {
+	if (/^"?Resources"?\s*:/.exec(template)) {
 		if (/"?Type"?\s*:\s*"?'?(AWS|Custom)::/.exec(template)) {
 			// filter out serverless.io templates
 			if (!(/\nresources:/.exec(template) && /\nprovider:/.exec(template))) {
