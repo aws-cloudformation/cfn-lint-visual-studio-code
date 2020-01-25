b2939ccb9240fd060e72d69e255f3773a823a87a
Pat Myron <pmyron@nd.edu>
2020-01-25 14:34:01 -0800

    CloudFormation Template Schema 10.4.0
    https://github.com/aws-cloudformation/aws-cloudformation-template-schema/blob/master/docs/tool/instructions.md

--- client/src/yaml-support/yaml-constant.ts
+++ client/src/yaml-support/yaml-constant.ts
@@ -6,6 +6,4 @@ export const CLOUDFORMATION_SCHEMA_PREFIX = CLOUDFORMATION_SCHEMA + '://schema/'
 
 export const VSCODE_YAML_EXTENSION_ID = 'redhat.vscode-yaml';
 
-export const CLOUDFORMATION_SCHEMA_VERSION = '2.15.0';
-
-export const CLOUDFORMATION_SCHEMA_FILE = path.join(__dirname, `../../../schema/spec-${CLOUDFORMATION_SCHEMA_VERSION}.json`);
+export const CLOUDFORMATION_SCHEMA_FILE = path.join(__dirname, `../../../schema/all-spec.json`);
