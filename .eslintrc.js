f76d7dea893f12f9b31936d16bfdc35e0e061323
Kevin DeJong <kddejong@amazon.com>
2020-03-27 09:04:23 -0500

    switch from tslint to eslint

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "parser": "@typescript-eslint/parser",
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "ignorePatterns": ["server/node_modules/", "client/node_modules/"],
+    "rules": {
+        "@typescript-eslint/class-name-casing": "warn",
+        "@typescript-eslint/member-delimiter-style": [
+            "warn",
+            {
+                "multiline": {
+                    "delimiter": "semi",
+                    "requireLast": true
+                },
+                "singleline": {
+                    "delimiter": "semi",
+                    "requireLast": false
+                }
+            }
+        ],
+        "@typescript-eslint/semi": [
+            "warn",
+            "always"
+        ],
+        "curly": "warn",
+        "eqeqeq": [
+            "warn",
+            "always"
+        ],
+        "no-redeclare": "warn",
+        "no-throw-literal": "warn",
+        "no-unused-expressions": "warn"
+    }
+};
