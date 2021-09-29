fbed6ee098ca770a4053500bc8839ab8da5ae350
Pat Myron <pat.myron@stedi.com>
2021-09-28 19:44:52 -0700

    patching template JSON schema

--- scripts/patch-schema.ts
+++ scripts/patch-schema.ts
@@ -1,5 +1,8 @@
 import { readFileSync } from "fs"
+import { applyPatch } from 'fast-json-patch'
 
-const schema = JSON.parse(readFileSync('schema.json', 'utf8'))
+const registrySchema = readFileSync('schema.json', 'utf8')
 const stub = readFileSync('schema/resource-patch-stub.json', 'utf8')
-console.log(stub.replace(/RESOURCE_TYPE/g, schema['typeName']).replace(/"RESOURCE_SCHEMA"/g, JSON.stringify(schema)))
+const patch = JSON.parse(stub.replace(/RESOURCE_TYPE/g, JSON.parse(registrySchema)['typeName']).replace(/"RESOURCE_SCHEMA"/g, registrySchema))
+let templateSchema = JSON.parse(readFileSync('schema/all-spec.json', 'utf8'))
+console.log(JSON.stringify(applyPatch(templateSchema, patch).newDocument))
