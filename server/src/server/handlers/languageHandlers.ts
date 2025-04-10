38f82e12a1d28ed4f5b6f6027a0064e08fb41cab
Kevin DeJong <kddejong@amazon.com>
2025-04-10 08:18:40 -0700

    Fix a prettier issue

--- server/src/server/handlers/languageHandlers.ts
+++ server/src/server/handlers/languageHandlers.ts
@@ -189,7 +189,9 @@ export class LanguageHandlers extends YamlLanguageHandlers {
    * Called when the formatter is invoked
    * Returns the formatted document content using prettier
    */
-  formatterHandler(formatParams: DocumentFormattingParams): Promise<TextEdit[]> {
+  formatterHandler(
+    formatParams: DocumentFormattingParams
+  ): Promise<TextEdit[]> {
     const uri = formatParams.textDocument.uri;
     const document = this.cfnSettings.documents.get(uri);
 
